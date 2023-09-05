598c95670d5dc784e57141cc1c2aec3e991e1a10
Jorge-1501 <jltg_1501@hotmail.com>
2023-09-05 11:18:19 -0600

    Documentación de network.py

--- Digitos/src/network.py
+++ Digitos/src/network.py
@@ -35,15 +35,31 @@ class Network(object):
         es una capa de entrada, y por convención no se establecen sesgos para esas neuronas,
         ya que los sesgos solo se utilizan en el cálculo de las salidas de las capas posteriores.
         """
+
+        # El número de capas en la red es igual al tamaño de la lista 'sizes'
         self.num_layers = len(sizes)
+
+        # 'sizes' es una lista que contiene el número de neuronas en cada capa
         self.sizes = sizes
+
+        # Inicialización aleatoria de biases para las capas ocultas y de salida.
+        # Se utiliza np.random.randn para generar valores aleatorios con distribución gaussiana.
+        # sizes[1:] omite la primera capa ya que no se establecen sesgos para las neuronas de entrada.
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+
+
+        # Inicialización aleatoria de pesos para las conexiones entre neuronas en capas.
+        # Se utiliza zip para combinar tamaños de capas consecutivas.
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
         """Devuelve la salida de la red si 'a' es la entrada."""
+        # Comienza un bucle que iterará a través de las capas de la red.    
         for b, w in zip(self.biases, self.weights):
+            # 1. Calcula el producto escalar (dot product) entre los pesos 'w' y la activación 'a' de la capa anterior.
+            # 2. Luego, suma el sesgo 'b' a ese producto escalar.
+            # 3. Luego, el resultado se pasa a través de una función de activación llamada 'sigmoid'.
             a = sigmoid(np.dot(w, a)+b)
         return a
 
@@ -54,16 +70,32 @@ class Network(object):
         entradas de entrenamiento y las salidas deseadas. Los otros parámetros son
         autoexplicativos. Si 'test_data' se proporciona, la red se evaluará con los datos de
         prueba después de cada época y se imprimirá el progreso parcial. Esto es útil para
-        realizar un seguimiento del progreso, pero ralentiza sustancialmente el proceso."""
+        realizar un seguimiento del progreso, pero ralentiza sustancialmente el proceso.
+        """
+        
+        # Si se proporciona 'test_data', se calcula la cantidad de ejemplos de prueba
         if test_data: n_test = len(test_data)
+        
+        # Se calcula la cantidad de ejemplos de entrenamiento
         n = len(training_data)
+        
+        # Se inicia un bucle que recorrerá el número especificado de épocas
         for j in range(epochs):
+            # Se barajan aleatoriamente los datos de entrenamiento en cada época
             random.shuffle(training_data)
+        
+            # Se divide el conjunto de datos de entrenamiento en mini batches
             mini_batches = [
                 training_data[k:k+mini_batch_size]
                 for k in range(0, n, mini_batch_size)]
+            
+            # Se itera a través de cada mini lote y se actualizan los pesos y sesgos de la red
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
+
+            # Si 'test_data' está presente, se evalúa el rendimiento de la red en los datos de prueba
+            # y se imprime el progreso parcial. Si no se proporcionan datos de prueba, se imprime el 
+            # progreso de la época.
             if test_data:
                 print("Época {0}: {1} / {2}".format(
                     j, self.evaluate(test_data), n_test))
@@ -74,16 +106,26 @@ class Network(object):
         """Actualiza los pesos y sesgos de la red aplicando el descenso de gradiente utilizando
         la retropropagación a un solo mini lote. El 'mini_batch' es una lista de tuplas '(x, y)',
         y 'eta' es la tasa de aprendizaje."""
+        # Inicializa los gradientes acumulativos de los sesgos y los pesos con ceros
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
+        
+        # Itera a través de cada par (x, y) en el mini batch
         for x, y in mini_batch:
+            # Calcula los gradientes de los sesgos y los pesos utilizando backpropagation
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+
+            # Acumula los gradientes en nabla_b y nabla_w utilizando la suma de vectores
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        # Actualiza los pesos de la red utilizando el descenso de gradiente
+        # Se multiplica cada gradiente (nabla_w) por la tasa de aprendizaje (eta) dividida por el tamaño del mini batch   
         self.weights = [w-(eta/len(mini_batch))*nw
                         for w, nw in zip(self.weights, nabla_w)]
+        # Actualiza los sesgos de la red de manera similar a los pesos
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
+
 
     def backprop(self, x, y):
         """Devuelve una tupla '(nabla_b, nabla_w)' que representa el gradiente para la función de
@@ -120,7 +162,11 @@ class Network(object):
     def evaluate(self, test_data):
         """Devuelve el número de entradas de prueba para las cuales la red neuronal produce el
         resultado correcto. Se asume que la salida de la red neuronal es el índice de la neurona
-        en la última capa con la activación más alta."""
+        en la última capa con la activación más alta.
+        """
+        # Utiliza la función 'feedforward' para calcular la salida de la red para cada entrada de prueba 'x'
+        # Luego, utiliza 'np.argmax' para encontrar el índice de la neurona con la activación más alta
+        # Esto se compara con la etiqueta 'y' correspondiente de la entrada de prueba
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
